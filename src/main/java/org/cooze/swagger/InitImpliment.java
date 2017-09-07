e910bd864dbfecab883404800ee73554101782df
xianzhe <xianzhe_song@dayouzc.com>
2017-09-07 10:49:37 +0800

    add springboot init impliment classes.

---
+++ src/main/java/org/cooze/swagger/InitImpliment.java
@@ -0,0 +1,17 @@
+package org.cooze.swagger;
+
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+
+/**
+ * @author xianzhe_song
+ * @version 1.0.0
+ * @desc
+ * @date 2017/9/7
+ */
+public class InitImpliment implements ApplicationContextInitializer {
+    @Override
+    public void initialize(ConfigurableApplicationContext configurableApplicationContext) {
+        //do something ....
+    }
+}
